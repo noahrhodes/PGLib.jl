87ed24ac13cf57022c6d6d95e0f5895d698fe18c
Noah Rhodes <nrhodes@wisc.edu>
2022-01-08 13:43:45 -0600

    Add warn info to pglib()

--- src/PGLIB.jl
+++ src/PGLIB.jl
@@ -2,7 +2,6 @@ module PGLIB
 
 using PowerModels
 
-
 function pglib(name::AbstractString)
     if isfile(joinpath(@__DIR__, "..", "data", name))
         case = PowerModels.parse_file(joinpath(@__DIR__, "..", "data", name))
@@ -15,7 +14,7 @@ function pglib(name::AbstractString)
         @info "opening case `pglib_opf_$name.m`"
         case = PowerModels.parse_file(joinpath(@__DIR__, "..", "data", "pglib_opf_$name.m"))
     else
-        @warn "Case `$(name)` was not found.  Try running func to find similar case names."
+        @warn "Case `$(name)` was not found.  Try running `find_pglib_case($name)` to find similar case names."
         return Dict{String,Any}()
     end
     return case
@@ -27,6 +26,10 @@ function find_pglib_case()
 end
 
 
+##TODO
+# - add sort on number of nodes (size)
+# - add sort on variation (api sad hvdc uc)
+# - add type, and show method.  make accessing list of files easy.
 function find_pglib_case(name::AbstractString)
     files = readdir(joinpath(@__DIR__, "..", "data"))
     filtered_files = filter(x-> occursin(name,x) , files)
@@ -37,3 +40,6 @@ end
 include("export.jl")
 
 end # module
+
+
+
